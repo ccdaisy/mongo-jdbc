a9500b3bef2b401360f2d5c6711de5bd7281b880
ccdaisy <daisy.li1989@gmail.com>
2010-09-17 09:20:21 +0800

    test update criteria preparestatement with integer

--- src/examples/java/blog.java
+++ src/examples/java/blog.java
@@ -6,45 +6,54 @@ import com.mongodb.jdbc.*;
 
 public class blog {
 
-    static void print( String name , ResultSet res )
-        throws SQLException {
-        System.out.println( name );
-        while ( res.next() ){
-            System.out.println( "\t" + res.getInt( "num" ) + "\t" + res.getString( "title" ) + "\t" + res.getObject( "tags" ) );
-        }
-    }
+	static void print(String name, ResultSet res) throws SQLException {
+		System.out.println(name);
+		while (res.next()) {
+			System.out.println("\t" + res.getInt("num") + "\t"
+					+ res.getString("title") + "\t" + res.getObject("tags"));
+		}
+	}
 
-    public static void main( String args[] )
-        throws SQLException , ClassNotFoundException {
-        
-        Class.forName( "com.mongodb.jdbc.MongoDriver" );
-        
-        Connection c = DriverManager.getConnection( "mongodb://localhost/exampledb" );
-        MongoConnection mc = (MongoConnection)c;
+	public static void main(String args[]) throws SQLException,
+			ClassNotFoundException {
 
-        Statement st = c.createStatement();
-        st.executeUpdate( "drop table blogposts" );
+		Class.forName("com.mongodb.jdbc.MongoDriver");
 
-        PreparedStatement ps = c.prepareStatement( "insert into blogposts ( title , tags , num ) values ( ? , ? , ? )" );
-        ps.setString( 1 , "first post!" );
-        ps.setObject( 2 , new String[]{ "fun" , "eliot" } );
-        ps.setInt( 3 , 1 );
-        ps.executeUpdate();
+		Connection c = DriverManager
+				.getConnection("mongodb://10.241.14.41:12340/exampledb");
+		MongoConnection mc = (MongoConnection) c;
 
-        ps.setString( 1 , "wow - this is cool" );
-        ps.setObject( 2 , new String[]{ "eliot" , "bar" } );
-        ps.setInt( 3 , 2 );
-        ps.executeUpdate();
-        ps.close();
+		Statement st = c.createStatement();
+		st.executeUpdate("drop table blogposts");
 
+		PreparedStatement ps = c
+				.prepareStatement("insert into blogposts ( title , tags , num ) values ( ? , ? , ? )");
+		ps.setString(1, "first post!");
+		ps.setObject(2, new String[] { "fun", "eliot" });
+		ps.setInt(3, 1);
+		ps.executeUpdate();
 
-        System.out.println( mc.getCollection( "blogposts" ).find().toArray() );
-        
-        print( "num should be 1 " , st.executeQuery( "select * from blogposts where tags='fun'" ) );
-        print( "num should be 2 " , st.executeQuery( "select * from blogposts where tags='bar'" ) );
+		ps.setString(1, "wow - this is cool");
+		ps.setObject(2, new String[] { "eliot", "bar" });
+		ps.setInt(3, 2);
+		ps.executeUpdate();
+		ps.close();
 
-        // TODO indexing
+		PreparedStatement psUpdate = c
+				.prepareStatement("update blogposts set tags=addtoset(?) where title='first post!'");
+		psUpdate.setInt(1, 5);
+		psUpdate.executeUpdate();
+		ps.close();
+		
+		System.out.println(mc.getCollection("blogposts").find().toArray());
 
-    }
+		print("num should be 1 ", st
+				.executeQuery("select * from blogposts where tags='fun'"));
+		print("num should be 2 ", st
+				.executeQuery("select * from blogposts where tags='bar'"));
 
-} 
+		// TODO indexing
+
+	}
+
+}
